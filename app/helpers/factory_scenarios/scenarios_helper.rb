6a438b1bcd79822b41640c048f7f5b8168fe183a
evil-c <clive.walmsley@gmail.com>
2013-08-01 15:17:46 -0400

    load the site in _self

--- app/helpers/factory_scenarios/scenarios_helper.rb
+++ app/helpers/factory_scenarios/scenarios_helper.rb
@@ -3,13 +3,13 @@ module FactoryScenarios
 
     def scenario_link(scenario)
       link_to "Login", enact_scenario_path(scenario),
-              :target => :dynamic, :id => "enact_#{scenario.name}",
+              :target => '_self', :id => "enact_#{scenario.name}",
               'data-open-in-iframe' => FactoryScenarios.config.open_in_iframe
     end
 
     def reload_scenario_link(scenario)
       link_to "Reset data & Login", enact_scenario_path(scenario, :clear => true),
-              :class => :reload, :target => :dynamic, :id => "reload_#{scenario.name}",
+              :class => :reload, :target => '_self', :id => "reload_#{scenario.name}",
               'data-open-in-iframe' => FactoryScenarios.config.open_in_iframe
     end
 
